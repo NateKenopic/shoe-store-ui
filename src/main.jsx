3a193bb72cabb25fbf29ce7e8e58d97911a0f02a
NateKenopic <nkenopic@outlook.com>
2024-07-02 21:37:36 -0400

    Updates

--- src/main.jsx
+++ src/main.jsx
@@ -3,33 +3,33 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Error from './components/Error/Error.jsx';
-import Mens from './components/Mens/Mens.jsx';
-import Login from './components/Login/Login.jsx';
-import CookieConsent from './components/Cookie-Consent/CookieConsent.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Error from './components/Error/Error.jsx'
+import Mens from './components/Mens/Mens.jsx'
+import Login from './components/Login/Login.jsx'
+// import CookieConsent from './components/Cookie-Consent/CookieConsent.jsx'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
     errorElement: <Error />
   },
   {
-    path: "/mens",
+    path: '/mens',
     element: <Mens />,
     errorElement: <Error />
   },
   {
-    path: "/login",
+    path: '/login',
     element: <Login />,
     errorElement: <Error />
   }
-]);
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}/>
     {/* <CookieConsent /> */}
-  </React.StrictMode>,
+  </React.StrictMode>
 )
